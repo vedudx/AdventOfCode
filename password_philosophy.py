aed2297e8fdd06288d40064939a8f4966240d06a
vedudx <53002423+vedudx@users.noreply.github.com>
2020-12-09 18:48:50 +0530

    Add files via upload

---
+++ password_philosophy.py
@@ -0,0 +1,27 @@
+file = open('day_2.txt', 'r')
+data = list(file.read().splitlines())
+
+count  = 0
+for i in data:
+    string = i.split()
+    pos_1,pos_2 = map(int, string[0].split('-'))
+    digit = string[1][0]
+    test = string[2]
+    x = digit == test[pos_1-1]
+    y = digit == test[pos_2-1]
+    if x ^ y:
+        count += 1
+#part1
+#for i in data:
+    #string = i.split()
+    #mini,maxi = map(int, string[0].split('-'))
+    #digit = string[1][0]
+    #test = string[2]
+    #x = test.count(digit)
+    #if x >= int(mini) and x <= int(maxi):
+        #count += 1
+        
+print(count)
+        
+    
+    
